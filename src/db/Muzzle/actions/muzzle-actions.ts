a9238596118111d5c612f72a8ab69b7673d666e0
Steven Freeman <sfreeman422@gmail.com>
2019-07-05 11:00:59 -0400

    Removed get functions from muzzle-actions. These will be addressed independent of saving data

--- src/db/Muzzle/actions/muzzle-actions.ts
+++ src/db/Muzzle/actions/muzzle-actions.ts
@@ -16,46 +16,6 @@ export function addMuzzleTransaction(
   return getRepository(Muzzle).save(transaction);
 }
 
-export function getAllMuzzleTransactions() {
-  return getRepository(Muzzle).find();
-}
-
-export function getAllMuzzlesByMuzzleId(muzzleId: string) {
-  return getRepository(Muzzle).find({ muzzledId: muzzleId });
-}
-
-export function getAllMuzzlesByRequestorId(requestorId: string) {
-  return getRepository(Muzzle).find({ requestorId });
-}
-
-export function getMostMuzzled() {
-  return "test";
-}
-
-export function getMostMuzzledByMessageSuppression() {
-  return getRepository(Muzzle)
-    .createQueryBuilder()
-    .where("");
-}
-
-export function getMostMuzzledByWordSuppression() {
-  return "test";
-}
-
-export function getMostMuzzledByCharacterSuppression() {
-  return "test";
-}
-
-export function getMostMuzzledByDate(date: Date) {
-  console.log(date);
-  return "test";
-}
-
-export function getMostMuzzledByDateRange(startDate: Date, endDate: Date) {
-  console.log(startDate, endDate);
-  return "test";
-}
-
 export function incrementMuzzleTime(transactionId: number, ms: number) {
   return getRepository(Muzzle).increment(
     { id: transactionId },
