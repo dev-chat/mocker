b5e0c3e966ca6da74835a488363fba18f24c602f
Steven Freeman <sfreeman422@gmail.com>
2019-07-06 18:50:51 -0400

    Added tests for defineService

--- src/services/define/define.service.spec.ts
+++ src/services/define/define.service.spec.ts
@@ -1,45 +1,44 @@
 import { IDefinition } from "../../shared/models/define/define-models";
-import {
-  capitalizeFirstLetter,
-  define,
-  formatDefs,
-  formatUrbanD
-} from "./define.service";
+import { DefineService } from "./define.service";
 
 describe("define-utils", () => {
+  let defineService: DefineService;
+
+  beforeEach(() => {
+    defineService = DefineService.getInstance();
+  });
+
   describe("capitalizeFirstLetter()", () => {
     it("should capitalize the first letter of a given string", () => {
-      expect(capitalizeFirstLetter("test string")).toBe("Test string");
+      expect(defineService.capitalizeFirstLetter("test string")).toBe(
+        "Test string"
+      );
     });
   });
 
   describe("define()", () => {
     it("should return a promise when attempting to define", () => {
-      expect(define("test")).toBeDefined(); // Firm this up
+      expect(defineService.define("test")).toBeDefined(); // Firm this up
     });
   });
 
   describe("formatDefs()", () => {
     it("should return an array of 3 length when no maxDefs parameter is provided", () => {
-      expect(formatDefs(testArray).length).toBe(3);
+      expect(defineService.formatDefs(testArray).length).toBe(3);
     });
 
     it("should return an array of 4 length when a maxDefs parameter of 4 is provided", () => {
-      expect(formatDefs(testArray, 4).length).toBe(4);
+      expect(defineService.formatDefs(testArray, 4).length).toBe(4);
     });
 
     it("should return testArray.length if maxDefs parameter is larger than testArray.length", () => {
-      expect(formatDefs(testArray, 10).length).toBe(5);
+      expect(defineService.formatDefs(testArray, 10).length).toBe(5);
     });
 
     it(`should return [{ "Sorry, no definitions found" }] if defArr === 0`, () => {
-      expect(formatDefs([])[0].text).toBe("Sorry, no definitions found.");
-    });
-  });
-
-  describe("formatUrbanD()", () => {
-    it("should return a formatted urbandictionary string", () => {
-      expect(formatUrbanD("A [way] to [test]")).toBe("A way to test");
+      expect(defineService.formatDefs([])[0].text).toBe(
+        "Sorry, no definitions found."
+      );
     });
   });
 });
