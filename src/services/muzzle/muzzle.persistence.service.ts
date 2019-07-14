2cc5f4da7ca388f800e3d37a498c5f3bc960a521
Steven Freeman <sfreeman422@gmail.com>
2019-07-13 21:42:39 -0400

    Adjusted query and commented out the typeorm calls

--- src/services/muzzle/muzzle.persistence.service.ts
+++ src/services/muzzle/muzzle.persistence.service.ts
@@ -258,22 +258,22 @@ export class MuzzlePersistenceService {
     }
 
     return getRepository(Muzzle).query(
-      `SELECT muzzle.*, 
+      `SELECT *, 
         count(muzzle.muzzledId) AS number_of_muzzled
         FROM muzzle LEFT JOIN muzzle
         ON (muzzle.requestorId = muzzle.muzzledId)
         GROUP BY muzzle.requestorId`
     );
 
-    return getRepository(Muzzle)
-      .createQueryBuilder("muzzle")
-      .select("muzzle.requestorId", "requestor")
-      .addSelect("muzzle.muzzledId", "opponent")
-      .addSelect("COUNT(*)", "nemesisCount")
-      .groupBy("muzzle.muzzledId")
-      .addGroupBy("muzzle.requestorId")
-      .orderBy("nemesisCount", "DESC")
+    // return getRepository(Muzzle)
+    //   .createQueryBuilder("muzzle")
+    //   .select("muzzle.requestorId", "requestor")
+    //   .addSelect("muzzle.muzzledId", "opponent")
+    //   .addSelect("COUNT(*)", "nemesisCount")
+    //   .groupBy("muzzle.muzzledId")
+    //   .addGroupBy("muzzle.requestorId")
+    //   .orderBy("nemesisCount", "DESC")
 
-      .getRawMany();
+    //   .getRawMany();
   }
 }
