92bcb9288403c1cb28c4555cc453501ee351e753
Steven Freeman <sfreeman422@gmail.com>
2019-07-24 17:39:00 -0400

    fixed other typo

--- src/services/muzzle/muzzle.persistence.service.ts
+++ src/services/muzzle/muzzle.persistence.service.ts
@@ -269,6 +269,7 @@ export class MuzzlePersistenceService {
       .addSelect("SUM(IF(muzzle.messagesSuppressed > 0, 1, 0))", "kills")
       .addSelect("muzzle.muzzledId")
       .addSelect("COUNT(*)", "deaths")
+      .addSelect("kills/deaths", "kdr")
       .groupBy("muzzle.requestorId")
       .orderBy("kdr", "DESC")
       .getRawMany();
