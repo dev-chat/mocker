a9da9fe5b186e633a90581a75d5c91bc28c16061
Steven Freeman <sfreeman422@gmail.com>
2020-02-01 09:58:31 -0500

    Added backfire and counter muzzle checking support to all controller

--- src/controllers/confession.controller.ts
+++ src/controllers/confession.controller.ts
@@ -1,4 +1,6 @@
 import express, { Router } from "express";
+import { BackFirePersistenceService } from "../services/backfire/backfire.persistence.service";
+import { CounterPersistenceService } from "../services/counter/counter.persistence.service";
 import { MuzzlePersistenceService } from "../services/muzzle/muzzle.persistence.service";
 import { WebService } from "../services/web/web.service";
 import { ISlashCommandRequest } from "../shared/models/slack/slack-models";
@@ -6,11 +8,17 @@ import { ISlashCommandRequest } from "../shared/models/slack/slack-models";
 export const confessionController: Router = express.Router();
 
 const muzzlePersistenceService = MuzzlePersistenceService.getInstance();
+const backfirePersistenceService = BackFirePersistenceService.getInstance();
+const counterPersistenceService = CounterPersistenceService.getInstance();
 const webService = WebService.getInstance();
 
 confessionController.post("/confess", (req, res) => {
   const request: ISlashCommandRequest = req.body;
-  if (muzzlePersistenceService.isUserMuzzled(request.user_id)) {
+  if (
+    muzzlePersistenceService.isUserMuzzled(request.user_id) ||
+    backfirePersistenceService.isBackfire(request.user_id) ||
+    counterPersistenceService.isCounterMuzzled(request.user_id)
+  ) {
     res.send(`Sorry, can't do that while muzzled.`);
   } else if (!request.text) {
     res.send("Sorry, you must send a message to confess.");
