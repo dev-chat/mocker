b6cd100ae93f59995effc65825fc3bef9b9a1dd3
Steven Freeman <sfreeman422@gmail.com>
2020-02-01 10:21:17 -0500

    Added support for users to check their rep

---
+++ src/controllers/reaction.controller.ts
@@ -0,0 +1,27 @@
+import express, { Router } from "express";
+import { BackFirePersistenceService } from "../services/backfire/backfire.persistence.service";
+import { CounterPersistenceService } from "../services/counter/counter.persistence.service";
+import { MuzzlePersistenceService } from "../services/muzzle/muzzle.persistence.service";
+import { ReactionService } from "../services/reaction/reaction.service";
+import { ISlashCommandRequest } from "../shared/models/slack/slack-models";
+
+export const repController: Router = express.Router();
+
+const muzzlePersistenceService = MuzzlePersistenceService.getInstance();
+const backfirePersistenceService = BackFirePersistenceService.getInstance();
+const counterPersistenceService = CounterPersistenceService.getInstance();
+const reactionService = new ReactionService();
+
+repController.post("/rep/get", async (req, res) => {
+  const request: ISlashCommandRequest = req.body;
+  if (
+    muzzlePersistenceService.isUserMuzzled(request.user_id) ||
+    backfirePersistenceService.isBackfire(request.user_id) ||
+    counterPersistenceService.isCounterMuzzled(request.user_id)
+  ) {
+    res.send(`Sorry, can't do that while muzzled.`);
+  } else {
+    const repValue = await reactionService.getRep(request.user_id);
+    res.send(repValue);
+  }
+});
