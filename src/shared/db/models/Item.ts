23d140fc23acaab3cda9af4508c2e55a3096d192
sfreeman422 <sfreeman422@protonmail.com>
2020-05-18 00:56:09 -0400

    Added user and item models

---
+++ src/shared/db/models/Item.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column()
+  public description!: string;
+
+  @Column()
+  public price!: number;
+}
