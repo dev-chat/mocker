4d4581058aa507008c47ace20b70daf00f7052fe
sfreeman422 <sfreeman422@protonmail.com>
2020-05-30 18:33:22 -0400

    Added DB_ITEM_SEED

---
+++ DB_ITEM_SEED.sql
@@ -0,0 +1,5 @@
+USE mockerdbdev;
+
+INSERT INTO `item` VALUES ("Quick Reload", "Muzzles refresh at 4x speed for the next two hours.", 1000);
+INSERT INTO `item` VALUES ("Heavy Hitter", "Adds an additional minute to your randomly generated muzzle time.", 1000);
+INSERT INTO `item` VALUES ("MOAB", "Muzzles everyone in the channel you specify.", 1000);
