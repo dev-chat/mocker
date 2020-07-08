f5a9b6700787a30f2acfe66eb5a26e967e848bb5
Steve Freeman <sfreeman422@protonmail.com>
2020-07-08 17:33:54 -0400

    Added checking for counter and support for global counter

--- src/controllers/counter.controller.ts
+++ src/controllers/counter.controller.ts
@@ -3,7 +3,6 @@ import { BackFirePersistenceService } from '../services/backfire/backfire.persis
 import { CounterPersistenceService } from '../services/counter/counter.persistence.service';
 import { CounterService } from '../services/counter/counter.service';
 import { MuzzlePersistenceService } from '../services/muzzle/muzzle.persistence.service';
-import { SlackService } from '../services/slack/slack.service';
 import { SlashCommandRequest } from '../shared/models/slack/slack-models';
 
 export const counterController: Router = express.Router();
@@ -11,13 +10,10 @@ export const counterController: Router = express.Router();
 const muzzlePersistenceService = MuzzlePersistenceService.getInstance();
 const backFirePersistenceService = BackFirePersistenceService.getInstance();
 const counterPersistenceService = CounterPersistenceService.getInstance();
-const slackService = SlackService.getInstance();
 const counterService = new CounterService();
 
 counterController.post('/counter', async (req, res) => {
   const request: SlashCommandRequest = req.body;
-  const userId = slackService.getUserId(request.text);
-  const counter = counterService.getCounterByRequestorAndUserId(userId, request.user_id);
   if (
     muzzlePersistenceService.isUserMuzzled(request.user_id) ||
     backFirePersistenceService.isBackfire(request.user_id) ||
@@ -29,12 +25,9 @@ counterController.post('/counter', async (req, res) => {
     );
   } else if (!request.text) {
     res.send('Sorry, you must specify who you would like to counter in order to use this service.');
-  } else if (counter) {
-    counterService.removeCounter(counter, true, request.channel_name);
-    res.send('Sorry, your counter has been countered.');
   } else {
     await counterService
-      .createCounter(userId, request.user_id)
+      .createCounter(request.user_id)
       .then(value => res.send(value))
       .catch(e => res.send(e));
   }
