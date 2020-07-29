8fbac2a15e5db21a71be189725305bc0affa93d9
sfreeman422 <sfreeman422@protonmail.com>
2020-07-29 19:43:47 -0400

    Added logging to saveReaction

--- src/services/reaction/reaction.persistence.service.ts
+++ src/services/reaction/reaction.persistence.service.ts
@@ -14,6 +14,7 @@ export class ReactionPersistenceService {
   private static instance: ReactionPersistenceService;
 
   public saveReaction(event: Event, value: number): Promise<Reaction> {
+    console.log(event.team);
     return new Promise(async (resolve, reject) => {
       const reaction = new Reaction();
       reaction.affectedUser = event.item_user;
