48962a55b1fab1374668c5277a349feb8ed30345
sfreeman422 <sfreeman422@protonmail.com>
2020-10-29 11:14:00 -0400

    Started work on allowing users to give rep

--- src/controllers/reaction.controller.ts
+++ src/controllers/reaction.controller.ts
@@ -2,11 +2,13 @@ import express, { Router } from 'express';
 import { SlashCommandRequest } from '../shared/models/slack/slack-models';
 import { SuppressorService } from '../shared/services/suppressor.service';
 import { ReactionReportService } from '../services/reaction/reaction.report.service';
+import { ReactionService } from '../services/reaction/reaction.service';
 
 export const reactionController: Router = express.Router();
 
 const suppressorService = new SuppressorService();
 const reportService = new ReactionReportService();
+const reactionService = new ReactionService();
 
 reactionController.post('/rep/get', async (req, res) => {
   const request: SlashCommandRequest = req.body;
@@ -15,5 +17,16 @@ reactionController.post('/rep/get', async (req, res) => {
   } else {
     const repValue = await reportService.getRep(request.user_id, request.team_id);
     res.send(repValue);
+  }
+});
+
+reactionController.post('/rep/give', async (req, res) => {
+  const request: SlashCommandRequest = req.body;
+  const [receivingUser, amount] = req.body.text.split(' ');
+  if (await suppressorService.isSuppressed(request.user_id, request.team_id)) {
+    res.send(`Sorry, can't do that while muzzled.`);
+  } else {
+    const giveRep = await reactionService.giveRep(request.user_id, receivingUser, amount, request.team_id);
+    res.send(giveRep);
   }
 });
