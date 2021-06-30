6299fceca75acb8e6bf75fd3e0e9213654ecdf63
Steve <sfreeman422@protonmail.com>
2021-06-30 12:12:31 -0400

    Removed need to hit API when language is english

--- src/shared/services/translation.service.ts
+++ src/shared/services/translation.service.ts
@@ -3,18 +3,22 @@ import Axios, { AxiosResponse } from 'axios';
 export class TranslationService {
   private supportedLanguages = ['zh-TW', 'en', 'eo', 'de', 'haw', 'ga', 'it', 'ja', 'la', 'ru', 'es', 'th', 'fr', 'he'];
   public translate(text: string): Promise<string> {
-    return Axios.post(
-      `https://translation.googleapis.com/language/translate/v2?key=${process.env.GOOGLE_TRANSLATE_API_KEY}`,
-      {
-        q: text,
-        source: 'en',
-        target: this.getRandomLanguage(),
-        format: 'text',
-      },
-    ).then((res: AxiosResponse) => {
-      console.log(res?.data?.data?.translations?.[0].translatedText);
-      return res?.data?.data?.translations?.[0].translatedText;
-    });
+    const lang = this.getRandomLanguage();
+    if (lang !== 'en') {
+      return Axios.post(
+        `https://translation.googleapis.com/language/translate/v2?key=${process.env.GOOGLE_TRANSLATE_API_KEY}`,
+        {
+          q: text,
+          source: 'en',
+          target: lang,
+          format: 'text',
+        },
+      ).then((res: AxiosResponse) => {
+        console.log(res?.data?.data?.translations?.[0].translatedText);
+        return res?.data?.data?.translations?.[0].translatedText;
+      });
+    }
+    return new Promise((resolve, reject) => resolve(text));
   }
 
   getRandomLanguage(): string {
