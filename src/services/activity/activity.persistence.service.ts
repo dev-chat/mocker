0eaf6e08d60fcd4f314f43abf27fc9e06fd01a0e
steve <sfreeman422@protonmail.com>
2021-07-16 16:08:40 -0400

    Removed per channel log

--- src/services/activity/activity.persistence.service.ts
+++ src/services/activity/activity.persistence.service.ts
@@ -88,7 +88,6 @@ export class ActivityPersistenceService {
       if (channel.name !== 'hot') {
         const averageMessage = parseInt(averageMessages?.find((x: any) => x.channel === channel.id)?.avg || 0);
         const currentMessage = parseInt(currentMessages?.find((x: any) => x.channel === channel.id)?.count || 0);
-        console.log(`${channel.id} - ${channel.name} - avg: ${averageMessage} - current: ${currentMessage}`);
         const channelTemp = {
           id: channel.id,
           name: channel.name,
