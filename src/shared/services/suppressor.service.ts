3f325e0b5934ac01d70eb512602353f0f0a18125
steve <sfreeman422@protonmail.com>
2021-08-16 11:31:28 -0400

    added log to debug

--- src/shared/services/suppressor.service.ts
+++ src/shared/services/suppressor.service.ts
@@ -235,6 +235,7 @@ export class SuppressorService {
       .format('YYYY-MM-DD HH:mm:ss');
     const end = moment().format('YYYY-MM-DD HH:mm:ss');
     const muzzles = await this.muzzlePersistenceService.getMuzzlesByTimePeriod(requestorId, teamId, start, end);
+    Object.keys(muzzles).forEach(key => console.log(key));
     console.log(`Number of muzzles for ${requestorId}: ${muzzles}`);
     const chanceOfBackfire = 0.05 + muzzles * 0.025;
     console.log(`Chance of Backfire for ${requestorId}: ${chanceOfBackfire}`);
