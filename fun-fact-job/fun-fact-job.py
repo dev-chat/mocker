a28e96794e9dd84ea6a0ded3a1fd1082a0dc8419
steve <sfreeman422@protonmail.com>
2021-12-08 12:46:33 -0500

    Changed channel to #general

--- fun-fact-job/fun-fact-job.py
+++ fun-fact-job/fun-fact-job.py
@@ -54,7 +54,7 @@ def sendSlackMessage(facts):
   try:
       response = client.api_call(
         api_method='chat.postMessage',
-        json={'channel': '#testbotz','text': message}
+        json={'channel': '#general','text': message}
       )
     
   except SlackApiError as e:
