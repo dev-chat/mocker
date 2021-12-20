f6d8ab4664ea812ddfba5711d49e7448882cc752
steve <sfreeman422@protonmail.com>
2021-12-20 17:36:43 -0500

    Changed confessions to call out the confessor

--- src/services/confession/confession.service.ts
+++ src/services/confession/confession.service.ts
@@ -8,12 +8,13 @@ export class ConfessionService {
   public confessionPersistenceService = ConfessionPersistenceService.getInstance();
 
   public async confess(requestorId: string, teamId: string, channelId: string, confession: string): Promise<void> {
-    const message = (await this.shouldBackfire(requestorId, teamId))
-      ? `<@${requestorId}> has confessed: 
-\`${confession}\``
-      : `Someone has confessed: 
-\`${confession}\``;
-    this.webService.sendMessage(channelId, message);
+    //     const message = (await this.shouldBackfire(requestorId, teamId))
+    //       ? `<@${requestorId}> has confessed:
+    // \`${confession}\``
+    //       : `Someone has confessed:
+    // \`${confession}\``;
+    //     this.webService.sendMessage(channelId, message);
+    this.webService.sendMessage(channelId, `:chicken: <@${requestorId}> :chicken: says: \`${confession}\``);
   }
 
   public async shouldBackfire(requestorId: string, teamId: string): Promise<boolean> {
