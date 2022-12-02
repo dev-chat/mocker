939d48aabe163367148889f455036dbcdd3c0c55
Steve Freeman <sfreeman422@protonmail.com>
2022-12-02 12:51:44 -0500

    Added support for AI text generation via OpenAI

---
+++ src/services/ai/ai.service.ts
@@ -0,0 +1,18 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+export class AIService {
+  private openai = new OpenAIApi(
+    new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    }),
+  );
+  public generateText(text: string): Promise<string | undefined> {
+    return this.openai
+      .createCompletion({
+        model: 'text-davinci-002',
+        prompt: text,
+        temperature: 0.6,
+      })
+      .then(x => x.data.choices[0].text);
+  }
+}
