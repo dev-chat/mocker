60af39ac0ce74a51382757b29d1537f4be821c3c
Steve Freeman <sfreeman422@protonmail.com>
2022-12-02 13:27:39 -0500

    Formatting fixes

--- src/services/ai/ai.service.ts
+++ src/services/ai/ai.service.ts
@@ -24,8 +24,8 @@ export class AIService {
       .then(x => {
         this.inflightRequests = this.inflightRequests.filter(x => x != user);
         console.log(x.data);
-        // Arbitrary 3 removes the first two /n characters in the response.
-        return x.data.choices[0].text?.slice(3);
+        // Arbitrary 2 removes the first two /n characters in the response.
+        return x.data.choices[0].text?.slice(2);
       });
   }
 }
