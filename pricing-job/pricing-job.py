1a34c0d497bd75a9d62dae2fe5d134e5ba640076
sfreeman422 <sfreeman422@protonmail.com>
2022-12-07 18:23:07 -0500

    removed comments and unnecessary logs

--- pricing-job/pricing-job.py
+++ pricing-job/pricing-job.py
@@ -35,7 +35,7 @@ mycursor.execute("SELECT id, pricePct from item;")
 print('Items retrieved!')
 
 items = mycursor.fetchall()
-print('Not implemented! Unable to get updated pricing because this logic needs to be revisited.')
+
 for team in teams:
   # get total earned rep by team per user.
   totalEarnedRepQuery= """SELECT SUM(value) as sum, affectedUser FROM reaction GROUP BY affectedUser ORDER BY sum DESC;"""
@@ -51,9 +51,7 @@ for team in teams:
   for totalSpent in totalRepSpent:
     repMap[totalSpent['user']] = repMap[totalSpent['user']] - totalSpent['sum']
   repMap = {key: val for key, val in sorted(repMap.items(), key = lambda ele: ele[1], reverse = True)}
-  print(repMap)
   medianIdx = math.floor((len(repMap) + 1 ) / 2)
-  print(medianIdx)
   repList = list(repMap.items())
   medianRep = repList[medianIdx][1]
   for item in items:
@@ -64,35 +62,5 @@ for team in teams:
     mycursor.execute(item_query)
     cnx.commit()
   print("Completed update for {team}".format(team=team['teamId']))
-# for team in teams:
-#   # get total earned rep by team
-#   # get total spent rep by team
-#   # get total available rep by math of above
-#     query = """SELECT AVG(middle_values) AS 'median' FROM (
-#   SELECT t1.rep AS 'middle_values' FROM
-#     (
-#       SELECT @row:=@row+1 as `row`, x.rep
-#       FROM rep AS x, (SELECT @row:=0) AS r
-#       WHERE teamId='{team}'
-#       ORDER BY x.rep
-#     ) AS t1,
-#     (
-#       SELECT COUNT(*) as 'count'
-#       FROM rep x
-#       WHERE teamId='{team}'
-#     ) AS t2
-#     WHERE t1.row >= t2.count/2 and t1.row <= ((t2.count/2) +1)) AS t3;""".format(team=team['teamId'])
-#     mycursor.execute(query)
-#     median = mycursor.fetchall()[0]['median']
-#     print("Median for {team} is {median}".format(team=team['teamId'], median=median))
-#     print("Updating items...")
-#     for item in items:
-#         item_id = item['id']
-#         team_id= team['teamId']
-#         price = float(median) * item['pricePct']
-#         item_query="INSERT INTO price(itemId, teamId, price, itemIdId) VALUES({item_id}, '{team_id}', {price}, {item_id});".format(item_id=item_id, team_id=team_id, price=price)
-#         mycursor.execute(item_query)
-#         cnx.commit()
-#     print("Completed update for {team}".format(team=team['teamId']))
 
 print("Completed job in {time} seconds!".format(time=time.time() - start))
