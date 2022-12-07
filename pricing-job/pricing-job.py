02d4a3505f4c3a3e87005e66b9d2bddd1896aa88
Steve Freeman <sfreeman422@protonmail.com>
2022-12-07 14:16:12 -0500

    Commented out code in the pricing-job so that it would not run. It was no longer correct based on changes to purchase and rep tables

--- pricing-job/pricing-job.py
+++ pricing-job/pricing-job.py
@@ -33,33 +33,36 @@ mycursor.execute("SELECT id, pricePct from item;")
 print('Items retrieved!')
 
 items = mycursor.fetchall()
-
-for team in teams:
-    query = """SELECT AVG(middle_values) AS 'median' FROM (
-  SELECT t1.rep AS 'middle_values' FROM
-    (
-      SELECT @row:=@row+1 as `row`, x.rep
-      FROM rep AS x, (SELECT @row:=0) AS r
-      WHERE teamId='{team}'
-      ORDER BY x.rep
-    ) AS t1,
-    (
-      SELECT COUNT(*) as 'count'
-      FROM rep x
-      WHERE teamId='{team}'
-    ) AS t2
-    WHERE t1.row >= t2.count/2 and t1.row <= ((t2.count/2) +1)) AS t3;""".format(team=team['teamId'])
-    mycursor.execute(query)
-    median = mycursor.fetchall()[0]['median']
-    print("Median for {team} is {median}".format(team=team['teamId'], median=median))
-    print("Updating items...")
-    for item in items:
-        item_id = item['id']
-        team_id= team['teamId']
-        price = float(median) * item['pricePct']
-        item_query="INSERT INTO price(itemId, teamId, price, itemIdId) VALUES({item_id}, '{team_id}', {price}, {item_id});".format(item_id=item_id, team_id=team_id, price=price)
-        mycursor.execute(item_query)
-        cnx.commit()
-    print("Completed update for {team}".format(team=team['teamId']))
+print('Not implemented! Unable to get updated pricing because this logic needs to be revisited.')
+# for team in teams:
+#   # get total earned rep by team
+#   # get total spent rep by team
+#   # get total available rep by math of above
+#     query = """SELECT AVG(middle_values) AS 'median' FROM (
+#   SELECT t1.rep AS 'middle_values' FROM
+#     (
+#       SELECT @row:=@row+1 as `row`, x.rep
+#       FROM rep AS x, (SELECT @row:=0) AS r
+#       WHERE teamId='{team}'
+#       ORDER BY x.rep
+#     ) AS t1,
+#     (
+#       SELECT COUNT(*) as 'count'
+#       FROM rep x
+#       WHERE teamId='{team}'
+#     ) AS t2
+#     WHERE t1.row >= t2.count/2 and t1.row <= ((t2.count/2) +1)) AS t3;""".format(team=team['teamId'])
+#     mycursor.execute(query)
+#     median = mycursor.fetchall()[0]['median']
+#     print("Median for {team} is {median}".format(team=team['teamId'], median=median))
+#     print("Updating items...")
+#     for item in items:
+#         item_id = item['id']
+#         team_id= team['teamId']
+#         price = float(median) * item['pricePct']
+#         item_query="INSERT INTO price(itemId, teamId, price, itemIdId) VALUES({item_id}, '{team_id}', {price}, {item_id});".format(item_id=item_id, team_id=team_id, price=price)
+#         mycursor.execute(item_query)
+#         cnx.commit()
+#     print("Completed update for {team}".format(team=team['teamId']))
 
 print("Completed job in {time} seconds!".format(time=time.time() - start))
