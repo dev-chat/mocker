1930e8a8be9cac0eb9000904c1196800adc11867
sfreeman422 <sfreeman422@protonmail.com>
2023-01-05 11:28:33 -0500

    Changed sendMessage to use MUZZLE_BOT_TOKEN

--- src/services/web/web.service.ts
+++ src/services/web/web.service.ts
@@ -89,7 +89,7 @@ export class WebService {
    * Handles sending messages to the chat.
    */
   public sendMessage(channel: string, text: string, blocks?: Block[] | KnownBlock[]): Promise<WebAPICallResult> {
-    const token: string | undefined = process.env.MUZZLE_BOT_USER_TOKEN;
+    const token: string | undefined = process.env.MUZZLE_BOT_TOKEN;
     const postRequest: ChatPostMessageArguments = {
       token,
       channel,
