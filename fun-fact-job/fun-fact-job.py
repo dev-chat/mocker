c937d25793900057a5cc8c9dbfd331a908266375
sfreeman422 <sfreeman422@protonmail.com>
2023-01-19 19:22:37 -0500

    Fixed url for getTrends

--- fun-fact-job/fun-fact-job.py
+++ fun-fact-job/fun-fact-job.py
@@ -52,7 +52,7 @@ def getQuote():
     }
 
 def getTrends():
-  url = "https://api.twitter.com/1.1/trends/place.json"
+  url = "https://api.twitter.com/1.1/trends/place.json?id=2459115"
   token = os.getenv("TWITTER_API_BEARER")
   trends = session.get(url, headers= { "Authorization": "Bearer {token}".format(token=token)})
   if (trends):
