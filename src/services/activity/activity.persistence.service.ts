871bbc5d5f62caec215fa757d9edc07d5b90e414
sfreeman422 <sfreeman422@protonmail.com>
2023-07-18 14:51:55 -0400

    Added bandaid to avoid workflows from breaking the service

--- src/services/activity/activity.persistence.service.ts
+++ src/services/activity/activity.persistence.service.ts
@@ -20,7 +20,11 @@ export class ActivityPersistenceService {
   private static instance: ActivityPersistenceService;
 
   async logActivity(request: EventRequest) {
-    console.log(request.event.user);
+    // This is a bandaid to stop workflows from breaking the service.
+    if (typeof request.event.user !== 'string') {
+      return;
+    }
+
     const user: SlackUser | undefined = await getRepository(SlackUser).findOne({
       slackId: request?.event?.user,
       teamId: request?.team_id,
