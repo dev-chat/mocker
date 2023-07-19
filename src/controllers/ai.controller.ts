94c268cc7dfc4dd15a739888413fbc0975d04138
sfreeman422 <sfreeman422@protonmail.com>
2023-07-19 12:13:41 -0400

    Fixed chunking to account for backtick characters

--- src/controllers/ai.controller.ts
+++ src/controllers/ai.controller.ts
@@ -49,7 +49,7 @@ aiController.post('/ai/text', async (req, res) => {
     }
 
     const blocks: KnownBlock[] = [];
-    const chunks = generatedText.match(/[\s\S]{1,2999}/g);
+    const chunks = generatedText.match(/[\s\S]{1,2986}/g);
     console.log(generatedText);
     console.log(chunks);
     if (chunks) {
