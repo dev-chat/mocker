d31a98f26cdf0f45d1ae7567cf0efc95373ee1f6
Steve Freeman <sfreeman422@protonmail.com>
2023-07-18 20:14:35 -0400

    Data is structured properly now to train. Need to test chatterbot library:

--- history-upload-job/job.py
+++ history-upload-job/job.py
@@ -5,9 +5,8 @@ rootdir = "./data"
 
 dataByChannel = {}
 
-for subdir, dirs, files in os.walk(rootdir):
-    # this doesnt seem to be going in order
-    for file in files:
+for subdir, dirs, files in sorted(os.walk(rootdir)):
+    for file in sorted(files):
         f = open(os.path.join(subdir, file))
         data = json.load(f)
         lastIdx = subdir.rfind('/')
@@ -16,13 +15,15 @@ for subdir, dirs, files in os.walk(rootdir):
 
         # this is just insane but basically gives back only the text from the messages
         data = list(map(lambda x: x["text"], list(filter(lambda x: x['type'] == 'message' and 'subtype' not in x, data))))
-
         # somehow this is only giving a single day of data rather than all of the data for the given channel
         if dataByChannel.get(channel) is None:
           dataByChannel[channel] = data
         else:
-          map(lambda x: dataByChannel[channel].append(x), data)
+          for item in data:
+            dataByChannel[channel].append(item)
+          print(len(dataByChannel[channel]))
         f.close()
 
 conversationsByChannel = open("conversationsByChannel_text_only.json", "w")
-conversationsByChannel.write(json.dumps(dataByChannel))
+conversationsByChannel.write(json.dumps(dataByChannel, indent=4))
+
