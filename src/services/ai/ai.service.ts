9eb87eab6f5076d11e260931103e9a5238dfe2d0
sfreeman422 <sfreeman422@protonmail.com>
2024-01-26 14:52:35 -0500

    Upgraded to turbo preview

--- src/services/ai/ai.service.ts
+++ src/services/ai/ai.service.ts
@@ -35,7 +35,7 @@ export class AIService {
 
     return this.openai.chat.completions
       .create({
-        model: 'gpt-4',
+        model: 'gpt-4-turbo-preview',
         messages: [{ role: 'system', content: text }],
         user: `${userId}-DaBros2016`,
       })
@@ -112,7 +112,7 @@ export class AIService {
     const prompt = `please give ${isDaily ? 'a summary' : 'a one sentence summary'} of the following conversation, followed by a three verbatims from the participants that are particularly funny or interesting ensuring that the verbatims are from different people.`;
     return this.openai.chat.completions
       .create({
-        model: 'gpt-4',
+        model: 'gpt-4-turbo-preview',
         messages: [
           {
             role: 'system',
