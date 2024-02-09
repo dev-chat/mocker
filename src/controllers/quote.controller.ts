ba1f535d20354b6c560502f1622dfe61266db75b
sfreeman422 <sfreeman422@protonmail.com>
2024-02-09 14:44:44 -0500

    Added toFixed(2) for marketcap

--- src/controllers/quote.controller.ts
+++ src/controllers/quote.controller.ts
@@ -73,7 +73,7 @@ const createQuoteBlocks = (quote: QuoteData, userId: string): Block[] | KnownBlo
         },
         {
           type: 'mrkdwn',
-          text: `*Market Cap*: $${(quote.marketCap / 1000000).toFixed(0)}T`,
+          text: `*Market Cap*: $${(quote.marketCap / 1000000).toFixed(2)}T`,
         },
         {
           type: 'mrkdwn',
