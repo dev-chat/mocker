bbdfcebde5c8c4b38f62aa757f0c887dd5c70b30
sfreeman422 <sfreeman422@protonmail.com>
2024-02-25 09:34:48 -0500

    changed verbiage and emoji for health job (#142)

--- health-job/job.py
+++ health-job/job.py
@@ -35,7 +35,7 @@ def sendSlackMessage():
   try:
       client.api_call(
         api_method='chat.postMessage',
-        json={'channel': '#muzzlefeedback','text': ':siren: MUZZLE IS DOWN!! :siren:'}
+        json={'channel': '#muzzlefeedback','text': ':this-is-fine: Muzzle returned a non-200 status code... :this-is-fine:'}
       )
     
   except SlackApiError as e:
