6e102fbb597eb9748668203687c6fe155bd7cdd5
sfreeman422 <sfreeman422@protonmail.com>
2024-02-29 13:48:13 -0500

    Reduced time to 5 minutes

--- src/services/activity/activity.persistence.service.ts
+++ src/services/activity/activity.persistence.service.ts
@@ -58,7 +58,7 @@ export class ActivityPersistenceService {
       console.log('refreshing latest hotness');
       this.refreshTime = false;
       // Every ffteen minutes.
-      setTimeout(() => (this.refreshTime = true), 900000);
+      setTimeout(() => (this.refreshTime = true), 300000);
       const hottest: Temperature[] = await this.getHottestChannels();
       console.log(hottest);
       const images: Promise<ChannelImage>[] = [];
