1a78c7df86f9524c0a112a40ca839b69b94495fd
sfreeman422 <sfreeman422@protonmail.com>
2025-04-28 11:47:50 -0400

    Removed confession persistence and updated controller

--- src/confession/confession.controller.ts
+++ src/confession/confession.controller.ts
@@ -1,21 +1,16 @@
 import express, { Router } from 'express';
-import { SlashCommandRequest } from '../shared/models/slack/slack-models';
-import { SuppressorService } from '../shared/services/suppressor.service';
 import { ConfessionService } from './confession.service';
+import { suppressedMiddleware } from '../shared/middleware/suppression';
+import { textMiddleware } from '../shared/middleware/textMiddleware';
 
 export const confessionController: Router = express.Router();
+confessionController.use(suppressedMiddleware);
+confessionController.use(textMiddleware);
 
 const confessionService = new ConfessionService();
-const suppressorService = new SuppressorService();
 
 confessionController.post('/confess', async (req, res) => {
-  const request: SlashCommandRequest = req.body;
-  if (await suppressorService.isSuppressed(request.user_id, request.team_id)) {
-    res.send(`Sorry, can't do that while muzzled.`);
-  } else if (!request.text) {
-    res.send('Sorry, you must send a message to confess.');
-  } else {
-    confessionService.confess(request.user_id, request.team_id, request.channel_id, request.text);
-    res.status(200).send();
-  }
+  res.status(200).send();
+  const { user_id, team_id, channel_id, text } = req.body;
+  confessionService.confess(user_id, team_id, channel_id, text);
 });
--- src/confession/confession.service.ts
+++ src/confession/confession.service.ts
@@ -1,23 +1,15 @@
 
 import { SlackService } from '../shared/services/slack/slack.service';
 import { WebService } from '../shared/services/web/web.service';
-import { ConfessionPersistenceService } from './confession.persistence.service';
 
 export class ConfessionService {
   public webService = WebService.getInstance();
   public slackService = SlackService.getInstance();
-  public confessionPersistenceService = ConfessionPersistenceService.getInstance();
 
   public async confess(requestorId: string, teamId: string, channelId: string, confession: string): Promise<void> {
     console.log(`${requestorId} - ${teamId} attempted to confess ${confession} in ${channelId}`);
     this.webService
       .sendMessage(channelId, `:chicken: <@${requestorId}> :chicken: says: \`${confession}\``)
       .catch(e => console.error(e));
-  }
-
-  public async shouldBackfire(requestorId: string, teamId: string): Promise<boolean> {
-    const confessions = await this.confessionPersistenceService.logConfession(requestorId, teamId);
-    const chanceOfBackfire = confessions * 0.05;
-    return Math.random() <= chanceOfBackfire;
   }
 }
