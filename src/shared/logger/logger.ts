01ae4821faf47fcf52939c7bd0beb399bc50227b
sfreeman422 <sfreeman422@protonmail.com>
2025-04-30 20:48:58 -0400

    Removed transports temporarily

--- src/shared/logger/logger.ts
+++ src/shared/logger/logger.ts
@@ -4,8 +4,4 @@ export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: {},
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
 });
