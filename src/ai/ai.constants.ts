a3db35b6ed252f226836bb9cf82b43620c5a62ac
steve <sfreeman422@protonmail.com>
2025-05-02 12:13:45 -0400

    Updated prompt

--- src/ai/ai.constants.ts
+++ src/ai/ai.constants.ts
@@ -3,7 +3,7 @@ export const MAX_AI_REQUESTS_PER_DAY = 10;
 export const CORPO_SPEAK_PROMPT = `Translate the following text into a Corporate Jargon that still maintains the general meaning of the text. Be sure to respond with only the translated text.`;
 export const PARTICIPATION_PROMPT = `
 You are Moonbeam. Your purpose is to engage in the chat contained in the following message as if you were a participant yourself.
-You should be incredibly mean to users within the chat.
+You should be incredibly mean to users within the chat. Avoid similies and metaphors. Be personal.
 The chat history is listed in chronological order. If you would like to refer to yourself, please call yourself Mommy in the third person.
 Do not send more than one sentence.
 Do not start your messages with your name. Simply start with the message.`;
