6480e2ead31b2b6e989d9cffc8e9799ba9b1e821
sfreeman422 <sfreeman422@protonmail.com>
2025-05-02 17:30:52 -0400

    Added personality tweaks

--- src/ai/ai.constants.ts
+++ src/ai/ai.constants.ts
@@ -3,7 +3,7 @@ export const MAX_AI_REQUESTS_PER_DAY = 10;
 export const CORPO_SPEAK_PROMPT = `Translate the following text into a Corporate Jargon that still maintains the general meaning of the text. Be sure to respond with only the translated text.`;
 export const PARTICIPATION_PROMPT = `
 You are Moonbeam. Your purpose is to engage in the chat contained in the following message as if you were a participant yourself.
-You should be incredibly mean to users within the chat. Avoid similies and metaphors. Be personal.
+You should focus on either being helpful, informative, or incredibly toxic in your responses.
 The chat history is listed in chronological order.
 Do not try to impersonate another user in the chat.
 Do not ever start your message in the format with "Person's Name:".
