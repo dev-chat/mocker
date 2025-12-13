fdced77dfde4d00d1d059a5a37f6b9bbba6c915d
Steve Freeman <sfreeman422@protonmail.com>
2025-12-13 11:11:27 -0500

    Converted to a much simpler schema

--- src/hook/hook.controller.ts
+++ src/hook/hook.controller.ts
@@ -1,23 +1,17 @@
 import express, { Router } from 'express';
 import { WebService } from '../shared/services/web/web.service';
 
-interface HookContent {
-  key?: string;
-  value?: unknown;
-}
-
 export const hookController: Router = express.Router();
 const webService = new WebService();
 const hookLogger = webService.logger.child({ module: 'HookController' });
 
 hookController.post('/', async (req, res) => {
-  const { body } = req;
-  if (!body?.length) {
-    hookLogger.info(`Invalid request: missing body.`);
-    res.status(400).send('Sorry, your request is missing a body.');
+  const { message } = req.body;
+  if (!message?.length) {
+    hookLogger.info(`Invalid request: missing message.`);
+    res.status(400).send('Sorry, your request is missing a message.');
     return;
   }
-  const message = body.map((item: HookContent) => `${item?.key}: ${item?.value}`).join('\n');
 
   webService
     .sendMessage('#products', message)
