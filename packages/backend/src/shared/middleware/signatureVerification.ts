d26f1fad3c440ec88f1357d3cb56eac82cc3849c
Steve Freeman <sfreeman422@protonmail.com>
2026-05-19 10:49:48 -0400

    Removed hook support

--- packages/backend/src/shared/middleware/signatureVerification.ts
+++ packages/backend/src/shared/middleware/signatureVerification.ts
@@ -36,7 +36,7 @@ export const signatureVerificationMiddleware = (req: Request, res: Response, nex
   midLogger.info('Received signature: ', { slackSignature });
   midLogger.info('Is valid Slack signature: ', { isValidSlackSignature });
 
-  if (isValidSlackSignature || req.headers.authorization === process.env.HOOK_TOKEN) {
+  if (isValidSlackSignature) {
     next();
   } else {
     midLogger.error('Someone is hitting your service from outside of slack.');
